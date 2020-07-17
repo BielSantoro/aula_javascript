31baf19d7b9405772443941fa610873439090293
Gabriel Santoro <gabrielssantoro@gmail.com>
2020-07-17 15:07:39 -0300

    Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-var nome = "Gabriel";
-alert("Bem vindo " + nome);
+/* var lista = ["maça", "pera", "laranja"];
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.join(" "));*/
+
+var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta[1].nome);
