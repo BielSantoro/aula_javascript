cb7b163539b6875c58d2209286d53919b375c296
Gabriel Santoro <gabrielssantoro@gmail.com>
2020-07-17 15:30:39 -0300

    Condicionais, laços e data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var idade = prompt("Digite sua idade");
+if (idade >= 18){
+    alert("Maior de idade");
+} else{
+    alert("Menor de idade");
+}
+
+
 /* var lista = ["maça", "pera", "laranja"];
 console.log(lista[1]);
 lista.push("uva");
@@ -6,7 +14,8 @@ lista.pop();
 console.log(lista);
 console.log(lista.length);
 console.log(lista.reverse());
-console.log(lista.join(" "));*/
+console.log(lista.join(" "));
 
 var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
 console.log(fruta[1].nome);
+*/
